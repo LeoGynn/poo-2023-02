458e9e7d8d2fb282436ae391e9fedc500e6ff6a4
LeoGynn <92831967+LeoGynn@users.noreply.github.com>
2023-10-07 20:53:05 -0300

    Exercicio E1.3 correção
    Corrigido a classe TestaLivro{} que não estava chamando o método main, por estar com um erro na posição dos "{}"

--- Livro.java
+++ Livro.java
@@ -51,8 +51,9 @@ public class Livro {
                 ", editora='" + editora + '\'' +
                 '}';
     }
-    public class TestaLivro{}
+    public static class TestaLivro{
         public static void main(String[] args) {
+
         Livro livro1 = new Livro("Pré-Escrita - Coleção Aprenda em Casa", "Vários Autores", 2015, "Todolivro");
         Livro livro2 = new Livro("Perigoso!", "Tim Warnes", 2014, "Ciranda Cultural");
         Livro livro3 = new Livro("O Show da Luna - Descobrindo os dinossauros", "Ciranda Cultural", 2018, "Ciranda Cultural");
@@ -65,4 +66,4 @@ public class Livro {
         System.out.println(livro3);
         }
     }
-    
+}
