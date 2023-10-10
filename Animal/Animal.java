23ded2ddcf1501d5548ab09e1c4f9a6a8dbce4ff
LeoGynn <92831967+LeoGynn@users.noreply.github.com>
2023-10-10 14:47:06 -0300

    Add files via upload

---
+++ Animal/Animal.java
@@ -0,0 +1,44 @@
+public class Animal{
+public String som;
+
+	public void setSom(String som) {
+		this.som = som;
+	}
+
+	public String getSom() {
+		return som; 
+	}
+	public class Sapo extends Animal {
+		@Override
+		public String som;
+		Animal somsapo = new som();
+	}
+
+
+	public class Cachorro extends Animal {
+		@Override
+		public String som;
+		Animal somcachorro = new som();
+		}
+	public class Boi extends Animal {
+		@Override
+		public String som;
+		Animal somBoi = new som();
+		}
+	}
+
+
+}
+class TestaAnimais{
+    public static void main(String[] args) {
+    	Animal[] animais = new Animal[3];
+    	animais[0] = new Sapo();
+    	animais[1] = new Cachorro();
+    	animais[2] = new Boi();
+    	
+    	class ExibirInformacoes {
+    	for (int i = 0; i < animais.3(); i++);
+        System.out.println(Animal[]);
+    	}
+    }	
+}
---
+++ Animal/poo-2023-02/README.md
@@ -0,0 +1,3 @@
+# poo-2023-02
+Aluno: Leonardo Ribeiro
+Numero-matricula: 202108689
